129f0fc1b7b19d78f6603c9d7e96637dbbe7a4a5
Jephonie <jephoniedominic.sanchez@ustp.edu.ph>
2025-10-21 10:31:45 +0800

    Update

--- LED_Flash.py
+++ LED_Flash.py
@@ -6,7 +6,7 @@ import adafruit_vl53l0x
 import RPi.GPIO as GPIO
 
 # ==== SETTINGS ====
-SERVER_URL = "http://192.168.100.15:5050/api/sensor"  # ✅ your Node/SvelteKit backend IP
+SERVER_URL = "http://192.168.100.1:5050/api/sensor"  # ✅ your Node/SvelteKit backend IP
 DISTANCE_THRESHOLD = 500  # mm
 NO_PERSON_TIMEOUT = 10    # seconds before stop
 LED_PIN = 17             # GPIO pin for LED
